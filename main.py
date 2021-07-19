9aa82e5c6e4e4c002e784e6cd69be34766921e41
jmares <67288966+jmares@users.noreply.github.com>
2021-07-19 17:45:35 +0200

    f strings all te way

--- main.py
+++ main.py
@@ -5,45 +5,46 @@ import sys
 import os
 from config import *
 import logging
-from datetime import datetime, date
-#from time import time
+from datetime import datetime
 import time
 
-def create_connection(db_file):
+def create_dbconnection(db_file):
     """ 
     Create a database connection to the SQLite database specified by db_file
     :param db_file: path to database file
     :return: SQLite3 connection or None
     """
     this_function = sys._getframe().f_code.co_name
-    logging.debug(this_function + ': start')
+    logging.debug(f"{this_function}: start")
     conn = None
 
     try:
         conn = sqlite3.connect(db_file)
-        logging.debug(this_function + ': connected to database ' + db_file)
+        logging.debug(f"{this_function}: connected to database {db_file}")
         return conn
     except Error as e:
-        logging.critical(this_function + ': could not connect to database: ' + e)
+        logging.critical(f"{this_function}: could not connect to database - {e}")
         sys.exit(1)
 
     return conn
 
 
 def get_users(twitcon, user_type):
-    """ Get list with user ids for Twitter friends or followers
+    """ 
+    Get list with user ids for Twitter friends or followers
     :param twitcon: Twitter connection
     :param user_type: Twitter user type (friends or followers)
     :return: list with Twitter ids
     """
     this_function = sys._getframe().f_code.co_name
     logging.debug(f"{this_function} - {user_type}")
+
     if user_type == 'friends':
         resp = twitcon.friends.ids(count=5000)
     elif user_type == 'followers':
         resp = twitcon.followers.ids(count=5000)
     else:
-        msg = f"Unknow Twitter user type. Valid types: friends and followers. Error type: {str(user_type)}"
+        msg = f"Unknow Twitter user type: {str(user_type)}. Valid types: friends and followers."
         logging.error(msg)
         raise Exception(msg)
 
@@ -55,7 +56,7 @@ def update_users(dbcon, twitcon, user_type):
     Update Twitter users list: insert if new user and update when existing user
     :param dbcon: Database connection
     :param twitcon: Twitter connection
-    :param user_type: User type (friends or followers)
+    :param user_type: User type (friends or followers), specifies table in database
     :return: 
     """
     this_function = sys._getframe().f_code.co_name
@@ -77,12 +78,15 @@ def update_users(dbcon, twitcon, user_type):
 
         if cur.execute(sql_test, (id, )).fetchone():
             logging.debug(f"{this_function} {user_type}: update id {str(id)}")
-            sql = f'''UPDATE {user_type} SET last_date = datetime(CURRENT_TIMESTAMP, 'localtime') WHERE twitter_id = ?'''
+            sql = f'''UPDATE {user_type} 
+                SET last_date = datetime(CURRENT_TIMESTAMP, 'localtime') 
+                WHERE twitter_id = ?'''
             cur.execute(sql, (id, ))
         else:
             logging.debug(f"{this_function} {user_type}: insert id {str(id)}")
             sql = f'''INSERT INTO {user_type} (twitter_id, start_date, last_date)
-                VALUES(?, datetime(CURRENT_TIMESTAMP, 'localtime'), datetime(CURRENT_TIMESTAMP, 'localtime'))'''
+                VALUES(?, datetime(CURRENT_TIMESTAMP, 'localtime'), 
+                datetime(CURRENT_TIMESTAMP, 'localtime'))'''
             cur.execute(sql, (id, ))
 
         dbcon.commit()
@@ -102,7 +106,7 @@ def get_metadata_users(dbcon, twitcon, user_type):
     logging.debug(f"{this_function} - {user_type}")
     cur = dbcon.cursor()
 
-    # Test if the follower already exists
+    # Get users with missing metadata (currently only screen-name)
     sql = f'''SELECT twitter_id FROM {user_type} WHERE screen_name IS NULL'''
 
     res = cur.execute(sql)
@@ -111,11 +115,11 @@ def get_metadata_users(dbcon, twitcon, user_type):
             user = twitcon.users.lookup(user_id=twitter_id)
             logging.debug(f"{this_function} - {user_type}: {str(user[0]['id'])} {user[0]['screen_name']}")
             add_metadata_user(dbcon, user[0], user_type)
-        except TwitterHTTPError as err:
-            logging.error(f"{this_function} - {user_type}: exceeded rate limit -  {err}")
+        except TwitterHTTPError as e:
+            logging.error(f"{this_function} - {user_type}: exceeded rate limit {e}")
             break
         except Exception as e:
-            logging.error(f"{this_function} - {user_type}: unexpected error -  {e}")
+            logging.error(f"{this_function} - {user_type}: unexpected error {e}")
             continue
 
 
@@ -146,13 +150,13 @@ def get_log_file_mode(log_file):
     now = datetime.now()
     # check if log-file exists 
     if os.path.isfile(log_file):
-        # if the log-file exists, compare the file modified date to the current date
+        # if the log-file exists, compare the modified date to the current date
         file_date = time.strftime("%Y-%m-%d", time.localtime(os.path.getmtime(log_file)))
         if file_date == now.strftime("%Y-%m-%d"):
-            # if the modified date of the log file equals the current date, set file-mode to append
+            # if the modified date equals the current date, set file-mode to append
             file_mode = 'a'
         else:
-            # if the log-file is different (older), set file-mode to (over)write
+            # if the modified date is different (older), set file-mode to (over)write
             file_mode = 'w'
     else:
         # if the log-file doesn't exist, set file-mode to write
@@ -173,20 +177,20 @@ def main():
     file_mode = get_log_file_mode(log_file)
     logging.basicConfig(filename=log_file, filemode=file_mode, format='%(asctime)s - %(levelname)s : %(message)s', level=log_level)
 
-    logging.info(this_function + ': start')
+    logging.info(f"{this_function}: start")
 
     # create a SQLite database connection
     try:
-        dbconn = create_connection(DB_FILE)
+        dbconn = create_dbconnection(DB_FILE)
     except Exception as e:
-        logging.critical(this_function + ': could not connect to database: ' + e)
+        logging.critical(f"{this_function}: could not connect to database: {e}")
         sys.exit(1)
     
     # create a Twitter connection
     try:
         twitconn = Twitter(auth=OAuth(TOKEN, TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
     except Exception as e:
-        logging.critical(this_function + ': could not connect to Twitter: ' + e)
+        logging.critical(f"{this_function}: could not connect to Twitter: {e}" )
         sys.exit(1)
 
     update_users(dbconn, twitconn, 'followers')
@@ -195,8 +199,8 @@ def main():
     get_metadata_users(dbconn, twitconn, 'friends')
 
     end_time = time.time()
-    duration = 'time needed for script: ' + str(round(end_time - start_time, 3)) + ' seconds\n'
-    logging.info(duration)
+    duration = str(round(end_time - start_time, 3))
+    logging.info(f"time needed for script: {duration} seconds\n")
     logging.debug('ending execution\n')
 
 
